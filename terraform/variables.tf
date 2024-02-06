47512d841d3eed3b69ca65d48a0917150433d054
leimiged <104398618+leimiged@users.noreply.github.com>
2024-02-06 22:21:56 +0100

    Segunda subida
    Codigo adicionando as maquinas e com seus ip pulblicos.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,6 +12,26 @@ variable "virtual_machines" {
   ]
 }
 
+variable "os_types" {
+  description = "Map of OS types for each virtual machine"
+  type        = map(string)
+  default     = {
+    ubuntu = "publicIPUbuntu"
+    debian = "publicIPDebian"
+    centos = "publicIPCentOS"
+  }
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  for_each = var.os_types
+
+  name                = each.value
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+
 variable "resource_group_name" {
   default = "ansible_lab"
 }
